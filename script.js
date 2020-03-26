a3c0985e39ce1e7618937e22828bea89387f4fab
Alex Younger <alexanderyounger@gmail.com>
2020-03-26 15:36:16 -0400

    Finished. Kinda.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-/* let pWins = 0;
+//set score
+let pWins = 0;
 let cWins = 0;
+//prep elements to be appended when the score is tallied
+let body = document.querySelector("body");
+let resultCounter = document.createElement("div");
+resultCounter.setAttribute("id", "resultcounter");
+let pWinCount = document.createElement("span");
+let cWinCount = document.createElement("span");
+pWinCount.setAttribute("class", "wincount");
+cWinCount.setAttribute("class", "wincount");
+//RNG computer play
 function computerPlay () {
     numGen = Math.floor(Math.random() * 3);
     switch(true){
@@ -11,65 +21,75 @@ function computerPlay () {
             break;
         default:
             return "scissors"; 
+        }
     }
-}
-function humanPlay () {
-    let decision = prompt("Choose rock, paper, or scissors:");
-    decision = decision.toLowerCase();
-        if (decision == "rock" || decision == "paper" || decision == "scissors") {
-        return decision;
-    } 
-    else { 
-    alert("Invalid selection");
-    decision = null;
-    }
-}
+//game mechanics
 function playRound (playerSelection, computerSelection) {
+    let gameWindow = document.querySelector("#rules");
+    //feedback on what happened
+    gameWindow.innerHTML = `<p>You throw ${playerSelection}. Computer throws ${computerSelection}.</p>`
+    //if it's the first throw, make a result window and insert it where the rules were
+    if (document.querySelector("#result") == null){
+        let result = document.createElement("div");
+        result.setAttribute("id", "result");
+        gameWindow.parentNode.insertBefore(result, gameWindow.nextSibling);
+    }
     if (playerSelection == computerSelection){
-        return `You both threw ${playerSelection}. It's a tie!`;
+        result.textContent = `It's a tie!`;
     } else if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
             pWins++;
-            return "You win! Rock crushes scissors.";
+            result.textContent = "You win! Rock crushes scissors.";
         } else {
             cWins++;
-            return "You lose. Paper covers rock.";
+            result.textContent = "You lose. Paper covers rock.";
         }
     }
     else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
             pWins++;
-            return "You win! Paper covers rock.";
+            result.textContent = "You win! Paper covers rock.";
         } else {
             cWins++;
-            return "You lose. Scissors cut paper.";
+            result.textContent = "You lose. Scissors cut paper.";
         }
     }
     else if (playerSelection == "scissors") {
         if (computerSelection == "paper") {
             pWins++;
-            return "You win! Scissors cut paper.";
+            result.textContent = "You win! Scissors cut paper.";
         } else {
             cWins++;
-            return "You lose. Rock crushes scissors.";
+            result.textContent = "You lose. Rock crushes scissors.";
         }
     }
     else {
         return "error";
     }
-}
-
-function game () {
-    for (i=0; i<5; i++) {
-        console.log(playRound(humanPlay(), computerPlay()));
+    //log the current win count
+    pWinCount.textContent = `Your wins: ${pWins}`;
+    cWinCount.textContent = `Computer wins: ${cWins}`;
+    //if it's the end of the first round, make a window to display the win counter
+    if (pWins + cWins === 1){
+        body.appendChild(resultCounter);
+        resultCounter.appendChild(pWinCount);
+        resultCounter.appendChild(cWinCount);
     }
-    
-    if (pWins > cWins) {
-        console.log(`You win, ${pWins} to ${cWins}!`);
-    } else if (pWins < cWins) {
-        console.log(`You lose, ${pWins} to ${cWins}.`);
-    } else {
-        console.log(`It's a tie, ${pWins} to ${cWins}`);
+    //win and lose condition with a reload to try again
+    if (pWins == 5){
+        alert(`You win, ${pWins} to ${cWins}! Rematch?`)
+        location.reload();
+    }
+    if (cWins == 5){
+        alert(`You lose, ${pWins} to ${cWins}. Try again!`)
+        location.reload();
     }
 }
-game (); */
+//player selection, and begin the game.
+const icons = document.querySelectorAll('.icon');
+[...icons].forEach(item => {
+    item.addEventListener("click", (e) => {
+        const playerSel = e.currentTarget.getAttribute("data-key");
+        playRound(playerSel, computerPlay());
+    });
+});
